00ddcd31e1b62b02b0431604969aafdb87adeaad
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2018-01-16 16:09:27 +0530

    Clock Angle Calculator

---
+++ coding_ninja_prep/src/com/porwau/interviewquestions/ClockAngleCalculator.java
@@ -0,0 +1,23 @@
+package com.porwau.interviewquestions;
+
+import java.util.Arrays;
+
+public class ClockAngleCalculator {
+
+	static float findAngle(String time) {
+		String[] timeParts = time.split(":");
+		int hrs = Integer.parseInt(timeParts[0]);
+		int minutes = Integer.parseInt(timeParts[1]);// parseInt gives int, valueOf gives Integer
+		float deltaHrDegree = minutes * .5F;
+		if (hrs == 12 && minutes == 0) {
+			minutes = 60;
+		}
+		return Math.abs(minutes * 6 - (hrs * 30 + deltaHrDegree));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String time = "1:59";
+		System.out.println("Angle b/w the clock hands at " + time + " is " + Math.abs(findAngle(time)) + " degree.");
+	}
+}
