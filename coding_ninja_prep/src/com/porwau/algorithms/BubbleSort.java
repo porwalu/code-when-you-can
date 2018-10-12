0980d5e573d5f5c2f47f6f981279deccf818876c
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2018-10-12 13:01:00 +0530

    Bubble Sort

---
+++ coding_ninja_prep/src/com/porwau/algorithms/BubbleSort.java
@@ -0,0 +1,16 @@
+package com.porwau.algorithms;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int[] intArr = {0,1,12,1,23,45,23};
+		bubbleSort(intArr);
+
+	}
+
+	private static void bubbleSort(int[] intArr) {
+		return;		
+	}
+
+}
