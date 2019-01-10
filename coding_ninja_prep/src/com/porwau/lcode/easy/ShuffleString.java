1ab53c9d5e0651558b84ec801546fb38650a3200
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2019-01-10 15:09:27 +0530

    lcode Easy Shuffle String

--- coding_ninja_prep/src/com/porwau/lcode/easy/ShuffleString.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/ShuffleString.java
@@ -2,17 +2,19 @@ package com.porwau.lcode.easy;
 
 public class ShuffleString {
 
-    private static String restoreString(String s, int[] indices) {
-        int length=indices.length;
-        char[] shuffleArray = new char[length];
-        for(int i=0;i<length;i++){
-            shuffleArray[indices[i]]=s.charAt(i);
-        }            
-        return String.valueOf(shuffleArray);
-    }
+	private static String restoreString(String s, int[] indices) {
+		int length = indices.length;
+		char[] shuffleArray = new char[length];
+		for (int i = 0; i < length; i++) {
+			shuffleArray[indices[i]] = s.charAt(i);
+		}
+		return String.valueOf(shuffleArray);
+	}
+
 	public static void main(String[] args) {
 		String reverseThis = "aaiougrt";
-		System.out.println(reverseThis + " is shuffled to : "+ restoreString(reverseThis, new int[]{4,0,2,6,7,3,1,5}));
+		System.out.println(
+				reverseThis + " is shuffled to : " + restoreString(reverseThis, new int[] { 4, 0, 2, 6, 7, 3, 1, 5 }));
 	}
 
 }
