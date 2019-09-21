0ef14660fc8b4f779e054fca58f68792fd48d6f2
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2019-09-21 13:01:00 +0530

    lcode easy fibonacci

--- coding_ninja_prep/src/com/porwau/lcode/easy/FibonacciSeries.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/FibonacciSeries.java
@@ -1,12 +1,20 @@
 package com.porwau.lcode.easy;
 
-//0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
+// Example of Fibonacci series - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
+/**
+ * @author Utkarsh Porwal
+ *
+ */
 public class FibonacciSeries {
 
 	private static void fibonacciRecursion(int n) {
 		return;
 	}
 
+	/**
+	 * @param n - integer n
+	 * @return the nth Term of fibonacci series
+	 */
 	private static int fibonacciIterative(int n) {
 		if (n < 3) {
 			if (n == 1) {
