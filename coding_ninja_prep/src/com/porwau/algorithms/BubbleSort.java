8dc6ba8c9bb94afcff66a195d5cb10f9be8ebea0
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2019-11-15 13:01:00 +0530

    Bubble Sort

--- coding_ninja_prep/src/com/porwau/algorithms/BubbleSort.java
+++ coding_ninja_prep/src/com/porwau/algorithms/BubbleSort.java
@@ -8,6 +8,8 @@ public class BubbleSort {
 
 		int[] intArr = { 78, 1, 12, 1, 23, 45, 23 };
 		bubbleSortDescending(intArr);
+		bubbleSortAscending(intArr);
+
 
 	}
 
@@ -24,8 +26,23 @@ public class BubbleSort {
 			}
 
 		}
+		System.out.println("Ascending Bubble Sort" + Arrays.toString(intArr));
+	}
+	
+	private static void bubbleSortAscending(int[] intArr) {
+		int arrLength = intArr.length;
+		int tmp = 0;
+		for (int i = 0; i < arrLength; i++) {
+			for (int j = i + 1; j < arrLength; j++) {
+				if (intArr[i] > intArr[j]) {
+					tmp = intArr[j];
+					intArr[j] = intArr[i];
+					intArr[i] = tmp;
+				}
+			}
 
-		System.out.println(Arrays.toString(intArr));
+		}
+		System.out.println("Ascending Bubble Sort" + Arrays.toString(intArr));
 	}
 
 }
