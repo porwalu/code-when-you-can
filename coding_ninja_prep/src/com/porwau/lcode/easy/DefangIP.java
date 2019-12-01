37963753f8beae3b8571a0157f55b58ecfdb410f
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2019-12-01 13:01:00 +0530

    lcode easy Defang IP

--- coding_ninja_prep/src/com/porwau/lcode/easy/DefangIP.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/DefangIP.java
@@ -1,7 +1,17 @@
 package com.porwau.lcode.easy;
 
+/**
+ * Defanged IP is one which has dots (.) of IP octets sorrouned with '[]'
+ * e.g. Defanged IP for 1.1.1.1 is 1[.]1[.]1[.]1
+ * @author Utkarsh Porwal
+ *
+ */
 public class DefangIP {
 
+    /**
+     * @param address - IP address in string format.
+     * @return - Defanged IP in char array
+     */
     private static char[] defangIPaddr(String address) {
         char []ch = new char[address.length()+6];
        int j = 0;
@@ -19,6 +29,6 @@ public class DefangIP {
     }
     
     public static void main(String[] args) {
-    	System.out.println("Defanged IP is " +String.valueOf(defangIPaddr("1.1.1.1")));
+    	System.out.println("Defanged IP is " +String.valueOf(defangIPaddr("11110.100.100.100")));
     }
 }
