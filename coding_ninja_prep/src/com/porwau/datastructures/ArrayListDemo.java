2fd5f1942329d5ab7f52663e90cc0a6cc8f08509
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2019-12-22 13:01:00 +0530

    Array List Demo

---
+++ coding_ninja_prep/src/com/porwau/datastructures/ArrayListDemo.java
@@ -0,0 +1,47 @@
+package com.porwau.datastructures;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		Collection<Integer> arrList = new ArrayList<Integer>();// default capacity is 10
+		
+		System.out.println("Default size is "+ arrList.size());// this doesn't return capacity but 
+															   //size of underlying array
+
+		ArrayList<Integer> arrList2 = new ArrayList<Integer>();
+		arrList2.add(2);
+		arrList2.add(3);
+
+		arrList.add(1);
+		System.out.println("Size now  is "+ arrList.size());
+		arrList.addAll(arrList2);//add a collection to the original array list.
+		System.out.println("Size now  is "+ arrList.size());
+		System.out.println("iS LIST impacted? "+ arrList.removeAll(arrList2));//add a collection to the original array list.
+		System.out.println("Size now  is "+ arrList.size());
+		System.out.println("iS LIST impacted? "+ arrList.removeAll(arrList2));//add a collection to the original array list.
+
+		arrList.clear();
+		System.out.println("Size now  is "+ arrList.size());
+		arrList.addAll(arrList2);
+		Iterator<Integer> iter = arrList.iterator();
+		while(iter.hasNext()){
+		//	iter.remove();//removes from underlying collection the value returned by next.
+		//	System.out.println(iter.next());
+			iter.next();
+		}
+
+		System.out.println("done");
+		System.out.println(arrList);
+
+
+
+
+
+		
+	}
+
+}
