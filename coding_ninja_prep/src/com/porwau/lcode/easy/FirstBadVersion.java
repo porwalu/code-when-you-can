cbce0b389a3cb543648ad75f1bd7a8d059e48c06
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-01-25 10:01:00 +0530

    lcode easy first bad version

--- coding_ninja_prep/src/com/porwau/lcode/easy/FirstBadVersion.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/FirstBadVersion.java
@@ -1,11 +1,10 @@
 package com.porwau.lcode.easy;
 
 /**
- * 278. First Bad Version
- * All the versions after the first bad versions are Bad.
- * We need to find first bad version
- * GGGGGGBBBB
- * Algo - Starting from the most recent version - find the version GB combination.
+ * 278. First Bad Version All the versions after the first bad versions are Bad.
+ * We need to find first bad version GGGGGGBBBB Algo - Starting from the most
+ * recent version - find the version GB combination.
+ * 
  * @author Utkarsh Porwal
  *
  */
@@ -16,30 +15,26 @@ public class FirstBadVersion {
 		findFirstBadLinear(versionCount);
 		findFirstBadLogarithimic(versionCount);
 
-
 	}
 
 	private static int findFirstBadLogarithimic(int versionCount) {
 		return versionCount;
-		
+
 	}
 
 	private static int findFirstBadLinear(int versionCount) {
 		int i = 0;
-		for(i = versionCount; i>0; i--) {
-            if(!isBadVersion(versionCount)){
-            	break;              
-            }
-        }
-        return i+1;
-
-			
+		for (i = versionCount; i > 0; i--) {
+			if (!isBadVersion(versionCount)) {
+				break;
+			}
 		}
+		return i + 1;
 
-private static boolean isBadVersion(int versionCount) {
-	// TODO Auto-generated method stub
-	return false;
+	}
+
+	private static boolean isBadVersion(int versionCount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
-}
-
-
