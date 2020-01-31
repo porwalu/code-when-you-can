d35b0cd513987dc9a44f412ef9f2797feab55e1b
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-01-31 10:01:00 +0530

    lcode easy first bad version

---
+++ coding_ninja_prep/src/com/porwau/lcode/easy/FirstBadVersion.java
@@ -0,0 +1,22 @@
+package com.porwau.lcode.easy;
+
+/**
+ * 278. First Bad Version
+ * 
+ * GGGGGGBBBB
+ * @author Utkarsh Porwal
+ *
+ */
+public class FirstBadVersion {
+
+	public static void main(String[] args) {
+		int versionCount = 5;
+		findFirstBad(versionCount);
+
+	}
+
+	private static void findFirstBad(int versionCount) {
+		
+	}
+
+}
