0a51e1379eadbe6bcfd58ebe16215ca4b50225b8
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-02-02 10:01:00 +0530

    lcode easy sqrt

--- coding_ninja_prep/src/com/porwau/lcode/easy/SqaureRoot.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/SqaureRoot.java
@@ -3,26 +3,25 @@ package com.porwau.lcode.easy;
 public class SqaureRoot {
 
 	public static void main(String[] args) {
-		int number = 100;
+		int number = 144;
 		System.out.println("Square root of " + number + " is " + sqrt(number));
 	}
 
 	private static int sqrt(int number) {
 		int left = 1;
 		int right = number;
-		while (right > left) {
-			int mid = left + (right - left) / 2;// square too wont be more than this.
+		int mid = 1;
+		while (left <=right) {
+			 mid = left + (right - left) / 2;// square too wont be more than this.
 
 			if (mid * mid > number) {
-				left = mid + 1;
+				right = mid -1;
 			} else if (mid * mid < number) {
-				// root is to the left of mid
-				right = mid;
-			} else {
-				return mid;// equal case
-			}
+				// root is to the right of mid
+				left = mid + 1;
+			}else {return mid;}
 		}
-		return 1;
+		return mid;
 	}
 
 }
