c706986701bfd289e5b6af9ee876b2b466dbe5f7
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-02-15 13:01:00 +0530

    ReverseStringFromStringInput

--- coding_ninja_prep/src/com/porwau/problemsolving/ReverseStringFromStringInput.java
+++ coding_ninja_prep/src/com/porwau/problemsolving/ReverseStringFromStringInput.java
@@ -1,7 +1,15 @@
 package com.porwau.problemsolving;
 
+/**This class is used to reverse a string while taking string (and not char array as input)
+ *  * @author Utkarsh Porwal
+ *
+ */
 public class ReverseStringFromStringInput {
 
+	/**
+	 * @param reverseThis - String to be reversed
+	 * Reversed string is also printed in the same method.
+	 */
 	private static void reverseString(String reverseThis) {
 
 		char[] strArr = reverseThis.toCharArray();
