a2f04ed9561cc54aab9fe0d0a1373922ba90a039
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-03-07 13:01:00 +0530

    lcode medium atoi

--- coding_ninja_prep/src/com/porwau/lcode/medium/ATOI.java
+++ coding_ninja_prep/src/com/porwau/lcode/medium/ATOI.java
@@ -4,15 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * ATOI stands for ASCII to Integer
- * Rules -
- * 1. Discards whitespaces.
- * 2.the first non whitespace should be integers.
- * 3. For invalid input or If no valid conversion could be performed, a zero value is returned.
- * Only the first integral part is considered. eg. for 42SDs32 - return 42 and not 32
- * Algo 
- * a) Check for valid inputs
- * b) grab only valid first integer and return.
+ * ATOI stands for ASCII to Integer Rules - 1. Discards whitespaces. 2.the first
+ * non whitespace should be integers. 3. For invalid input or If no valid
+ * conversion could be performed, a zero value is returned. Only the first
+ * integral part is considered. eg. for 42SDs32 - return 42 and not 32 Algo a)
+ * Check for valid inputs b) grab only valid first integer and return.
+ * 
  * @author Utkarsh Porwal
  *
  */
@@ -21,16 +18,16 @@ public class ATOI {
 	final static int defAtoi() {
 		return 1;
 	}
-	public static void main(String[] args) {
-		String pattern = "^(\\s*)([0-9]+)(.*)"; //0 or more spaces, followed by 1 0r more numbers, followed by anything.
-		String test = " ad12d";
 
-        if(Pattern.matches("^\\s*[0-9]+.*",test)) {
-        	System.out.println("valid");
-        }else {
-        	System.out.println("Invalid");
-        }
-		System.out.println(test);
+	public static void main(String[] args) {
+		String pattern = "^(\\s*)([0-9]+)(.*)"; // 0 or more spaces, followed by 1 0r more numbers, followed by
+												// anything.
+		String test = " 12d";
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(test);
+		if (m.matches()) {
+			System.out.println("Group 0 " + m.group(3));
+		}
 
 	}
 
