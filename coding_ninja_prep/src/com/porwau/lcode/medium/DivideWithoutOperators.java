138688f71daa38ef7113db938a09f1038a441b7c
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-05-11 13:01:00 +0530

    lcode medium divide without operator - remove long and abs

--- coding_ninja_prep/src/com/porwau/lcode/medium/DivideWithoutOperators.java
+++ coding_ninja_prep/src/com/porwau/lcode/medium/DivideWithoutOperators.java
@@ -9,58 +9,33 @@ package com.porwau.lcode.medium;
 public class DivideWithoutOperators {
 
 	public static void main(String[] args) {
-		int dividend = -2147483648;
+		int dividend = 10;
 		int divisor = -1109186033;
 
 		System.out.println("Quotient of division is " + divide(dividend, divisor));
 	}
 
 	public static int divide(int dividend, int divisor) {
-		if (divisor == dividend) {
-			return 1;
-		}
-		if (divisor == Integer.MIN_VALUE) {
-			return 0;
-		}
-		if (dividend == 0) {
-			return 0;
-		}
-		if (divisor == 1) {
+		if (dividend == 0 || divisor == 1) {
 			return dividend;
 		}
-		boolean divisorNegative = false;
-		boolean dividendNegative = false;
-		boolean dividendMin = false;
 
-		System.out.println("absolute value is " + Math.abs(-100));
-		if (dividend == Integer.MIN_VALUE) {
-			dividendMin = true;
+		if (dividend == Integer.MIN_VALUE && divisor == -1) {
+			return Integer.MAX_VALUE;
 		}
-		if (divisor == -1) {
-			return dividendMin ? -(dividend + 1) : -dividend;
+		int sign = dividend < 0 ^ divisor < 0 ? -1 : 1;
+		if (dividend > 0) {
+			dividend = -dividend;
 		}
-		int count = 0;
-		if (divisor < 0) {
-			divisorNegative = true;
+		if (divisor > 0) {
 			divisor = -divisor;
 		}
-		if (dividend < 0) {
-			dividendNegative = true;
-			dividend = dividendMin ? -(dividend + divisor) : -dividend;
-		}
-
-		if (divisor > dividend && !dividendMin) {// both negative or both positve.
-			return 0;
-		}
-
-		while (dividend >= divisor) {
+		int count = 0;
+		while (dividend <= divisor) {
 			dividend -= divisor;
 			count++;
 		}
-		if (dividendMin) {
-			count = count + 1;
-		}
 
-		return (dividendNegative || divisorNegative) && !(dividendNegative && divisorNegative) ? -count : count;
+		return sign == -1 ? -count : count;
 	}
 }
