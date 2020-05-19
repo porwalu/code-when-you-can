5363b01342e9d9fafe98684d65c33b6d204b1263
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-05-19 13:01:00 +0530

    lcode divide without operator

--- coding_ninja_prep/src/com/porwau/lcode/easy/DivideWithoutOperators.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/DivideWithoutOperators.java
@@ -3,49 +3,57 @@ package com.porwau.lcode.easy;
 public class DivideWithoutOperators {
 
 	public static void main(String[] args) {
-int dividend = -2147483648;
-int divisor = -1109186033;
+		int dividend = -2147483648;
+		int divisor = -1109186033;
 
-System.out.println("Quotient of division is " + divide(dividend,divisor));
+		System.out.println("Quotient of division is " + divide(dividend, divisor));
 	}
 
-	 public static int divide(int dividend, int divisor) {
-	        if(divisor == dividend){return 1;}
-	        if(divisor == Integer.MIN_VALUE){return 0;}
-	        if(dividend == 0){return 0;}
-	       if(divisor == 1) {
-		        	return dividend;
-		    }
-	        boolean divisorNegative = false;
-	        boolean dividendNegative = false;
-	        boolean dividendMin = false;
+	public static int divide(int dividend, int divisor) {
+		if (divisor == dividend) {
+			return 1;
+		}
+		if (divisor == Integer.MIN_VALUE) {
+			return 0;
+		}
+		if (dividend == 0) {
+			return 0;
+		}
+		if (divisor == 1) {
+			return dividend;
+		}
+		boolean divisorNegative = false;
+		boolean dividendNegative = false;
+		boolean dividendMin = false;
 
-	        if(dividend == Integer.MIN_VALUE){dividendMin=true;}
-	        if(divisor == -1) {
-		        	return dividendMin?-(dividend+1):-dividend;
-		        }
-	        int count = 0;
-	        if(divisor < 0 ){
-	            divisorNegative = true;
-	            divisor = -divisor;
-	        }
-	        if(dividend < 0){
-	            dividendNegative = true;
-	            dividend = dividendMin?-(dividend+divisor):-dividend;
-	        }
-	        
-	        if(divisor > dividend && !dividendMin){//both negative or both positve.
-	            return 0;
-	        }
-	        
-	        while(dividend >= divisor){
-	            dividend-= divisor;
-	            count++;
-	        }
-	        if(dividendMin){
-	            count = count +1;
-	        }
-	    
-	        return (dividendNegative||divisorNegative) && !(dividendNegative && divisorNegative) ?-count:count;
-	    }
+		if (dividend == Integer.MIN_VALUE) {
+			dividendMin = true;
+		}
+		if (divisor == -1) {
+			return dividendMin ? -(dividend + 1) : -dividend;
+		}
+		int count = 0;
+		if (divisor < 0) {
+			divisorNegative = true;
+			divisor = -divisor;
+		}
+		if (dividend < 0) {
+			dividendNegative = true;
+			dividend = dividendMin ? -(dividend + divisor) : -dividend;
+		}
+
+		if (divisor > dividend && !dividendMin) {// both negative or both positve.
+			return 0;
+		}
+
+		while (dividend >= divisor) {
+			dividend -= divisor;
+			count++;
+		}
+		if (dividendMin) {
+			count = count + 1;
+		}
+
+		return (dividendNegative || divisorNegative) && !(dividendNegative && divisorNegative) ? -count : count;
+	}
 }
