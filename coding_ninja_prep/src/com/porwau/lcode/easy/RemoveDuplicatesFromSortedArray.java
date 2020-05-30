8700dbb9c1f7743021a8b973ee7afec969e4980a
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-05-30 13:01:00 +0530

    lcode easy Longest Common Prefix

---
+++ coding_ninja_prep/src/com/porwau/lcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,29 @@
+package com.porwau.lcode.easy;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+
+	private static int removeDuplicates(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		int counter = 0;
+		int index = 0;
+		for (int i = 1; i < nums.length; i++) {
+			if(nums[index] != nums[i]) {
+			index += 1;
+			nums[index] = nums[i];
+			}
+		}
+		// nums[index ] = nums[nums.length -1];
+
+		return index + 1;
+	}
+
+	public static void main(String[] args) {
+		int[] numArray = { 1, 1, 2, 3, 3, 4, 4, 4, 4, 4 };
+		System.out.println(removeDuplicates(numArray) + Arrays.toString(numArray));
+	}
+
+}
