201ae330090d7b02f0ba922b252f4963d836c297
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-06-05 13:01:00 +0530

    lcode easy Longest Common Prefix

--- coding_ninja_prep/src/com/porwau/lcode/easy/LongestCommonPrefix.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/LongestCommonPrefix.java
@@ -2,14 +2,22 @@ package com.porwau.lcode.easy;
 
 public class LongestCommonPrefix {
 
-	public static void main(String[] args) {
-		int smallestWordIndex = 201;
-		String[] strings = { "flower", "flow", "flight" };
-		for (int i = 0; i< strings.length; i++) {
-			if(strings[i].length() < smallestWordIndex) {
-				smallestWordIndex = i;
+	private static String findPrefix(String[] strings){
+	       if(strings.length == 0){
+	            return "";
+	        }
+			String prefix = strings[0];
+			for (int i = 1; i < strings.length; i++) {
+				while (strings[i].indexOf(prefix) != 0) {
+					prefix = prefix.substring(0, prefix.length() - 1);
+				}
 			}
+			System.out.println(prefix);
+			return prefix;
 		}
-	}
-
+	
+	public static void main(String[] args) {
+		String[] strings = { "c","racecar","car" };
+		System.out.println("Longest Common Prefix is "+ findPrefix(strings));
+}
 }
