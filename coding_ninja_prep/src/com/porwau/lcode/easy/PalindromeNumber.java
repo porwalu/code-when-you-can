d84d2f045192075a4732818630fef5a402181ff9
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-06-22 13:01:00 +0530

    lcode easy check palindrome

--- coding_ninja_prep/src/com/porwau/lcode/easy/PalindromeNumber.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/PalindromeNumber.java
@@ -3,20 +3,20 @@ package com.porwau.lcode.easy;
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
-int num = 121;
-System.out.println("Is " + num + " a palindrome? " + isPalindrome(num));
+		int num = 121;
+		System.out.println("Is " + num + " a palindrome? " + isPalindrome(num));
 	}
 
 	private static boolean isPalindrome(int x) {
-        if(x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
-        }
-        int reverse = 0;
-        while (x > reverse){
-            reverse = reverse*10+x%10;
-            x=x/10;
-        }
-        return x == reverse || x == reverse/10 ;
+		if (x < 0 || (x % 10 == 0 && x != 0)) {
+			return false;
+		}
+		int reverse = 0;
+		while (x > reverse) {
+			reverse = reverse * 10 + x % 10;
+			x = x / 10;
+		}
+		return x == reverse || x == reverse / 10;
 
 	}
 
