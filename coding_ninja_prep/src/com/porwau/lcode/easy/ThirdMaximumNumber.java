bfc0827ace6654427f1fd0519dda18f5b6e6c98f
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-07-20 13:01:00 +0530

    lcode easy - third maximum number

--- coding_ninja_prep/src/com/porwau/lcode/easy/ThirdMaximumNumber.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/ThirdMaximumNumber.java
@@ -1,11 +1,12 @@
 package com.porwau.lcode.easy;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ThirdMaximumNumber {
-	static int ThirdMax(int[] arrNum) {
+	static int thirdMax(int[] arrNum) {
 		Set<Integer> maxSet = new HashSet<>();
 		for(int i = 0 ; i < arrNum.length; i++) {
 			maxSet.add(arrNum[i]);
@@ -20,6 +21,8 @@ public class ThirdMaximumNumber {
 		}
 	}
 	public static void main(String[] args) {
+		int[] arrNum = {2, 2, 1};
+		System.out.println("Third Maximum number of " + Arrays.toString(arrNum) + " is - " + thirdMax(arrNum));
 		
 	}
 
