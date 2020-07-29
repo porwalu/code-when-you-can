92033ed72c8597388164c70f8782c07efae430f5
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-07-29 13:01:00 +0530

    lcode easy - Binary to decimal via linked list

--- coding_ninja_prep/src/com/porwau/lcode/easy/BinaryToDecimalViaLinkedlist.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/BinaryToDecimalViaLinkedlist.java
@@ -1,9 +1,53 @@
 package com.porwau.lcode.easy;
 
+class Node{
+	public Node(int value) {
+		this.value = value;
+	}
+	Node next;
+	int value;
+	
+}
+class BinaryLinkedList{
+	
+	Node head;
+	void insert(int value){//just after head
+		Node newNode = new Node(value);
+		if(head == null) {
+			head = newNode;
+			head.next = null;
+		}else {
+			newNode.next = head.next;
+			head.next = newNode;
+		}
+		
+	}
+	
+}
+
 public class BinaryToDecimalViaLinkedlist {
 
+
+    static int getDecimalValue(Node head) {
+        Node currNode = head;
+        int decimalNum = 0;
+        //int counter = 0;
+      while(currNode !=null){
+          decimalNum = decimalNum*2+currNode.value;
+          currNode = currNode.next;
+      }
+        return decimalNum;
+        
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinaryLinkedList bll = new BinaryLinkedList();
+		bll.insert(1);
+		bll.insert(0);
+		bll.insert(1);
+		getDecimalValue(bll.head);
+
+		
 
 	}
 
