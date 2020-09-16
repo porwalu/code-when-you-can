2b8bef7aa8ccc8acf3d4857e0f88328c56902a67
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-09-16 13:01:00 +0530

    lcode easy - two sum - brute force

--- coding_ninja_prep/src/com/porwau/lcode/easy/TwoSum.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/TwoSum.java
@@ -5,13 +5,20 @@ import java.util.Arrays;
 public class TwoSum {
 
 	static int[] twoSum(int[] arrNum, int target) {
+		for(int i = 0; i < arrNum.length; i++) {
+			for ( int j = i + 1; j < arrNum.length; j++) {
+				if( arrNum[j] == target - arrNum[i]) {
+					return new int[] {i, j};
+				}
+			}
+		}
 		
-		return new int[] {1};
+		return new int[] {0};
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arrNum = {2,7,11,15};
-		int target = 9;
+		int[] arrNum = {3,3};
+		int target = 6;
 		System.out.println("Two sum which gives " +  target + " is " + Arrays.toString(twoSum(arrNum,target)));
 
 	}
