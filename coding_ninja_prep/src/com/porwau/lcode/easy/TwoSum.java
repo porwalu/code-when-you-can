92199581a21c5172a8ca04fa21f57a3e6691d44a
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-09-17 13:01:00 +0530

    lcode easy - two sum

---
+++ coding_ninja_prep/src/com/porwau/lcode/easy/TwoSum.java
@@ -0,0 +1,19 @@
+package com.porwau.lcode.easy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+	static int[] twoSum(int[] arrNum, int target) {
+		
+		return new int[] {1};
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arrNum = {2,7,11,15};
+		int target = 9;
+		System.out.println("Two sum which gives " +  target + " is " + Arrays.toString(twoSum(arrNum,target)));
+
+	}
+
+}
