3a9e6aeb85f3fdfdeaa489fac37b4ab5ee71dfff
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-09-21 13:01:00 +0530

    DoublyLinkedList

--- coding_ninja_prep/src/com/porwau/datastructures/DoublyLinkedListImpl.java
+++ coding_ninja_prep/src/com/porwau/datastructures/DoublyLinkedListImpl.java
@@ -1,5 +1,8 @@
 package com.porwau.datastructures;
 
+import java.util.LinkedList;
+
+//import java.util.Collections
 /**
  * Node has data, prev and next 1st node after Head, will have prev as null Last
  * node will have next as null/
@@ -7,16 +10,78 @@ package com.porwau.datastructures;
  * @author Utkarsh Porwal
  *
  */
-public class DoublyLinkedListImpl {
+
+class DoublyLinkedList<Integer> {
+
 	class Node {
-		int data;
-		Node next;
-		Node prev;
+		private int data;
+		private Node next;
+		private Node prev;
+
+		Node() {
+			System.out.println("Welcome to initialization");
+		}
+
+		Node(int data) {
+			this.data = data;
+		}
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private Node head;
+	private Node tail;
+	private int data;
+	private int length;
 
+	boolean addNode(int data) {// Just after head.
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			System.out.println("First ever node");
+			head = newNode;
+			// head.data=data;
+			head.next = null;
+			head.prev = null;
+			tail = head;
+			newNode = null;
+		}
+		// else {if(head == tail){//Only one node
+//			newNode.next = head.next;
+//			head.next.prev = newNode;
+//			head.next=newNode;
+//			newNode.prev = head;			
+//		}else {//there are many nodes.
+//			
+//		}
+
+		return true;
+	}
+
+	boolean removeNode() {
+		return true;
+	}
+
+	void printList() {
+		System.out.print("LinkedList content are - ");
+		Node curr = head;
+		while (curr != null) {
+			System.out.print("[" + curr.data + "]");
+			curr = curr.next;
+		}
+	}
+}
+
+public class DoublyLinkedListImpl {
+
+	public static void main(String[] args) {
+
+		DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+		dll.addNode(10);
+		dll.printList();
+		System.out.println();
+
+		LinkedList<Integer> ll = new LinkedList<>();
+		ll.add(1);
+		System.out.println("Size is " + ll.size());
 	}
 
 }
