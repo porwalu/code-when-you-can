8fc1c11bec85d4a0f510240a0b34d5308a513337
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-10-15 13:01:00 +0530

    Break Palindrome

--- coding_ninja_prep/src/com/porwau/lcode/easy/BreakPalindrome.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/BreakPalindrome.java
@@ -1,15 +1,16 @@
 package com.porwau.lcode.easy;
 
 /**
- * Assumption - all lowercase input characters.
- * Break a palindrome by addind a lowercase character to make result lexicographically smallest.
+ * Assumption - all lowercase input characters. Break a palindrome by addind a
+ * lowercase character to make result lexicographically smallest.
+ * 
  * @author Utkarsh Porwal
  *
  */
 public class BreakPalindrome {
 	// Test cases 1) odd and even lengths strings.
-	//all a's
-	//odd length string with all all 'a' except midpoint.
+	// all a's
+	// odd length string with all all 'a' except midpoint.
 
 	/**
 	 * @param palindrome - palindrome to be broken.
@@ -28,13 +29,10 @@ public class BreakPalindrome {
 			}
 		}
 		return palindrome.substring(0, strLength - 1) + 'b';
-
 	}
 
 	public static void main(String[] args) {
 		String str = "aa";
 		System.out.println("Break palindrome - " + breakPalindrome(str));
-
 	}
-
 }
