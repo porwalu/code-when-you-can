2895c4232bb5a0bede5c4391ad071c57b3d20d6a
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-10-21 13:01:00 +0530

    lcode medium - break a palindrome

---
+++ coding_ninja_prep/src/com/porwau/lcode/easy/BreakPalindrome.java
@@ -0,0 +1,33 @@
+package com.porwau.lcode.easy;
+
+public class BreakPalindrome {
+
+    //even length --- replace the mid point with next lowercase.
+    //odd length -- replace the midpoint -1 / +1
+    //empty String
+	//all a's
+	//lexicographically smallest.
+	 
+	public static String breakPalindrome(String palindrome) {
+	        int strLength = palindrome.length();
+	        if (strLength == 1){
+	            return "";
+	        } 
+
+	        int midPoint = strLength /2 ;
+	        for (int i = 0; i < midPoint; i++) {
+	        	if(palindrome.charAt(i) != 'a') {
+	        		return palindrome.substring(0,i) + 'a' + palindrome.substring(i+1,strLength);
+	        	}
+
+	        }
+	        		return 'b'+ palindrome.substring(1,strLength);
+	    }
+
+	public static void main(String[] args) {
+		String str = "aaaaaaaaa";
+		System.out.println("Break palindrome - "+breakPalindrome(str));
+
+	}
+
+}
