47bf5abf19d75690334fe764504b3f7931f81c49
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-10-23 13:01:00 +0530

    lcode easy Running Sum 1d

--- coding_ninja_prep/src/com/porwau/lcode/easy/RunningSum1DArray.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/RunningSum1DArray.java
@@ -2,19 +2,25 @@ package com.porwau.lcode.easy;
 
 import java.util.Arrays;
 
+/**
+ * @author Utkarsh Porwal
+ *
+ */
 public class RunningSum1DArray {
-	
-    private static int[] runningSum(int[] nums) {
-        for (int i = 1 ; i < nums.length; i++){
-            nums[i] = nums[i]+nums[i-1];
-        }
-        return nums;
-        
-    }
 
-	public static void main(String[] args) {
-		int[] intNum = {1,2,3,4,5};
-		System.out.println("Running Sum is "+ Arrays.toString(runningSum(intNum)));
+	/**
+	 * @param nums - input array of integers
+	 * @return running sum as integer array
+	 */
+	private static int[] runningSum(int[] nums) {
+		for (int i = 1; i < nums.length; i++) {
+			nums[i] = nums[i] + nums[i - 1];
+		}
+		return nums;
 	}
-
+	public static void main(String[] args) {
+		int[] intNum = { 1, 2, 3, 4, 5 };
+		System.out.println("Running Sum is " + Arrays.toString(runningSum(intNum)));
+	}
 }
+//Time complexity O(n), space complexity O(1)
