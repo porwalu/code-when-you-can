9f16167e9822bff6401d7ff98fe33725fa93cd70
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-11-02 13:01:00 +0530

    Fibonacci

---
+++ coding_ninja_prep/src/com/porwau/concepts/HashFunction.java
@@ -0,0 +1,15 @@
+package com.porwau.concepts;
+
+public class HashFunction {
+
+	int a;
+	String b;
+	public static void main(String[] args) {
+		HashFunction hf = new HashFunction();
+		
+		System.out.println(hf.hashCode());
+	
+		System.out.println(hf.toString());
+
+	}
+}
