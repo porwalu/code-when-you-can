7d3acc4a6f609c6670c280d578240450a93b06f3
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-11-03 13:01:00 +0530

    Bubble Sort

--- coding_ninja_prep/src/com/porwau/algorithms/BubbleSort.java
+++ coding_ninja_prep/src/com/porwau/algorithms/BubbleSort.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 /**
  * This class sorting array in ascending and descending order using Bubble sort.
- * Bubble sort. compare adjacent  elements with each other in multiple passes and bubble them up.
- * in each pass one element is finalized (biggest or smallest)
+ * Bubble sort. compare adjacent elements with each other in multiple passes and
+ * bubble them up. in each pass one element is finalized (biggest or smallest)
+ * 
  * @author Utkarsh Porwal
  *
  */
@@ -13,7 +14,7 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 
-		//Tip for string sorting. use compareTo method accordingly
+		// Tip for string sorting. use compareTo method accordingly
 		int[] intArr = { 78, 1, 12, 1, 23, 45, 23 };
 		bubbleSortDescending(intArr);
 		bubbleSortAscending(intArr);
@@ -21,42 +22,39 @@ public class BubbleSort {
 	}
 
 	/**
-	 * @param intArr -  Array of integer to be sorted in descending order using bubble sort.
+	 * @param intArr - Array of integer to be sorted in descending order using
+	 *               bubble sort.
 	 */
 	private static void bubbleSortDescending(int[] intArr) {
 		int arrLength = intArr.length;
 		int tmp = 0;
 		for (int i = 0; i < arrLength; i++) {// i doesn't have any role here. it it used to create n iterations.
 			for (int j = 0; j < arrLength - 1; j++) {
-				if (intArr[j] < intArr[j+1]) {
+				if (intArr[j] < intArr[j + 1]) {
 					tmp = intArr[j];
-					intArr[j] = intArr[j+1];
-					intArr[j+1] = tmp;
+					intArr[j] = intArr[j + 1];
+					intArr[j + 1] = tmp;
 				}
 			}
-			System.out.println("Descending Bubble no. " + i + " " + Arrays.toString(intArr));
-
 		}
 		System.out.println("Descending Bubble Sort" + Arrays.toString(intArr));
 	}
 
 	/**
-	 * @param intArr - Array of integer to be sorted in ascending order using bubble sort.
+	 * @param intArr - Array of integer to be sorted in ascending order using bubble
+	 *               sort.
 	 */
 	private static void bubbleSortAscending(int[] intArr) {
 		int arrLength = intArr.length;
 		int tmp = 0;
 		for (int i = 0; i < arrLength; i++) {
-			for (int j = 0; j < arrLength -1; j++) {
-				if (intArr[j] > intArr[j+1]) {
+			for (int j = 0; j < arrLength - 1; j++) {
+				if (intArr[j] > intArr[j + 1]) {
 					tmp = intArr[j];
-					intArr[j] = intArr[j+1];
-					intArr[j+1] = tmp;
+					intArr[j] = intArr[j + 1];
+					intArr[j + 1] = tmp;
 				}
 			}
-			System.out.println("Ascending Bubble no. " + i + " " + Arrays.toString(intArr));
-
-
 		}
 		System.out.println("Ascending Bubble Sort" + Arrays.toString(intArr));
 	}
