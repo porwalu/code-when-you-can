783739442964af34a5cc8cc60c39f8378ab45889
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-11-07 10:01:00 +0530

    lcode easy best time to buy and sell stocks

--- coding_ninja_prep/src/com/porwau/lcode/easy/BuyAndSellStocks.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/BuyAndSellStocks.java
@@ -3,10 +3,10 @@ package com.porwau.lcode.easy;
 public class BuyAndSellStocks {
 
 	public static void main(String[] args) {
-		System.out.println("Max profit is " + maxProfit(new int[] {7,6,4,3,1}));
+		System.out.println("Max profit is " + maxProfitBruteF(new int[] {7,6,4,3,1}));
 	}
 
-	private static int maxProfit(int[] prices) {
+	private static int maxProfitBruteF(int[] prices) {
 		int profit = 0;
 		for (int i = 0; i < prices.length;i++) {
 			for(int j = i+1;j< prices.length; j++) {
