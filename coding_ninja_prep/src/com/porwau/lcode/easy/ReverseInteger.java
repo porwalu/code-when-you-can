826140a9d0fdfcd81ec76ef60c62123ad0a8d67c
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-12-07 01:03:26 +0530

    lcode Easy ReverseInteger

--- coding_ninja_prep/src/com/porwau/lcode/easy/ReverseInteger.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/ReverseInteger.java
@@ -4,7 +4,8 @@ public class ReverseInteger {
 
 	private static int reverseInt(int reverseThis) {
 
-		long reversedNum = 0;
+		long reversedNum = 0;//This is long because in some cases we can have a reverse number 
+							//e.g 1534236469, exceeding the int range
 		while (reverseThis != 0) {
 			reversedNum = reversedNum * 10 + reverseThis % 10;
 			reverseThis = reverseThis / 10;
@@ -17,8 +18,10 @@ public class ReverseInteger {
 	}
 
 	public static void main(String[] args) {
-		int reverseThis=231313131;
+		int reverseThis=1534236469;// reverse of this is out of integer range.
 		System.out.println("Reverse of " + reverseThis + " is : "+reverseInt(reverseThis));
 	}
 
 }
+//Time complexity O(n)
+//Space Complexity. - O(1)?
