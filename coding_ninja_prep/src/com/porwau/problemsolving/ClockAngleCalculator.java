46ba0c6a40de3eda917d14b58c69ddc0ce43dc8e
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-12-08 21:07:06 +0530

    Clock Angle Calculator

--- coding_ninja_prep/src/com/porwau/problemsolving/ClockAngleCalculator.java
+++ coding_ninja_prep/src/com/porwau/problemsolving/ClockAngleCalculator.java
@@ -2,23 +2,40 @@ package com.porwau.problemsolving;
 
 import java.util.Arrays;
 
+/**
+ * This program takes time in hh:mm format and returns the angle in degree
+ * between the hour and min hand. <br>
+ * Assumptions : hh is between 1 - 12
+ * 
+ * @author Utkarsh Porwal
+ *
+ */
 public class ClockAngleCalculator {
 
-	static float findAngle(String time) {
+	/**
+	 * @param time - Time in hh:mm format
+	 */
+	static void findAngle(String time) {
 		String[] timeParts = time.split(":");
 		int hrs = Integer.parseInt(timeParts[0]);
 		int minutes = Integer.parseInt(timeParts[1]);// parseInt gives int, valueOf gives Integer
-		float deltaHrDegree = minutes * .5F;
+		if ((hrs < 1 || hrs > 12|| minutes < 0 || minutes > 59)) {//Checks invalid time
+			System.out.println("Invalid time!! Please enter in hh:mm format");
+			return;
+		}
+		float deltaHrDegree = minutes * .5F;//because hr hand would have moved depending on minutes elasped.
+		
 		if (hrs == 12 && minutes == 0) {
 			minutes = 60;
 		}
-		return Math.abs(minutes * 6 - (hrs * 30 + deltaHrDegree));
+		System.out.println("Angle b/w the clock hands at " + time + " is "
+				+ Math.abs(minutes * 6 - (hrs * 30 + deltaHrDegree)) + " degree.");
+
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String time = "1:59";
-		System.out.println("Angle b/w the clock hands at " + time + " is " + Math.abs(findAngle(time)) + " degree.");
+		String time = "12:60";
+		findAngle(time);
 	}
 }
 //Time  and Space complexity = O(1)
