538b0e37dce2222c011a62f1470ff8e828769409
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-12-10 00:25:40 +0530

    Longest word in string

--- coding_ninja_prep/src/com/porwau/problemsolving/LongestWordInString.java
+++ coding_ninja_prep/src/com/porwau/problemsolving/LongestWordInString.java
@@ -1,5 +1,13 @@
 package com.porwau.problemsolving;
 
+/**
+ * This class takes the sentence as an input and returns the longest word in the sentence.
+ * <br>Assumptions:
+ * <br><li>We will ignore anything other than [A-Za-z0-9\\s+] in the sentence.
+ * <br><li>Spaces(one or more) are taken as word delimeters
+ * @author Utkarsh Porwal
+ *
+ */
 public class LongestWordInString {
 
 	public static void main(String[] args) {
@@ -17,6 +25,9 @@ public class LongestWordInString {
 		longestWord(sentence);
 	}
 
+	/**
+	 * @param sentence - Sentence from which we need to print the longest word
+	 */
 	private static void longestWord(String sentence) {
 		String[] wordArr = sentence.split("\\s+");
 		int maxLength = 0;
