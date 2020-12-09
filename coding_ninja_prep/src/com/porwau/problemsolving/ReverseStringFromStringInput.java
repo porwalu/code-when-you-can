8d967d096689194544ae366c53837be09c4bd5de
Utkarsh Porwal <porwalutkarsh.iitbhu@gmail.com>
2020-12-09 13:37:14 +0530

    ReverseStringFromStringInput

--- coding_ninja_prep/src/com/porwau/problemsolving/ReverseStringFromStringInput.java
+++ coding_ninja_prep/src/com/porwau/problemsolving/ReverseStringFromStringInput.java
@@ -1,25 +1,29 @@
 package com.porwau.problemsolving;
 
-/**This class is used to reverse a string while taking string (and not char array as input)
- *  * @author Utkarsh Porwal
+/**
+ * This class is used to reverse a string while taking string (and not char
+ * array as input) * @author Utkarsh Porwal
  *
  */
 public class ReverseStringFromStringInput {
 
 	/**
-	 * @param reverseThis - String to be reversed
-	 * Reversed string is also printed in the same method.
+	 * @param reverseThis - String to be reversed Reversed string is also printed in
+	 *                    the same method.
 	 */
 	private static void reverseString(String reverseThis) {
 
-		char[] strArr = reverseThis.toCharArray();
+		// char[] strArr = reverseThis.toCharArray();//Sol 1
+		int strLen = reverseThis.length();
+		char[] strArr = new char[strLen];// Sol 2
 
 		char tmp = ' ';
-		int arrLength = strArr.length;
-		for (int i = 0; i < arrLength / 2; i++) {
-			tmp = strArr[i];
-			strArr[i] = strArr[arrLength - 1 - i];
-			strArr[arrLength - 1 - i] = tmp;
+		for (int i = 0; i < strLen; i++) {
+			/*
+			 * tmp = strArr[i]; strArr[i] = strArr[arrLength - 1 - i]; strArr[arrLength - 1-
+			 * i] = tmp;
+			 */
+			strArr[i] = reverseThis.charAt(strLen - i - 1);
 		}
 		System.out.println("'" + reverseThis + "' is reversed to : '" + String.copyValueOf(strArr) + "'");
 		return;
