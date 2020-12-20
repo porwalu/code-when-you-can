737e39fabc909c7a795b77ebbec58286d51bef36
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-20 22:41:58 +0530

    lcode easy - two sum - Optimized

--- coding_ninja_prep/src/com/porwau/lcode/easy/TwoSum.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/TwoSum.java
@@ -1,25 +1,43 @@
 package com.porwau.lcode.easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
-	static int[] twoSum(int[] arrNum, int target) {
-		for(int i = 0; i < arrNum.length; i++) {
-			for ( int j = i + 1; j < arrNum.length; j++) {
-				if( arrNum[j] == target - arrNum[i]) {
-					return new int[] {i, j};
+	private static int[] twoSum(int[] arrNum, int target) {
+		for (int i = 0; i < arrNum.length; i++) {
+			for (int j = i + 1; j < arrNum.length; j++) {
+				if (arrNum[j] == target - arrNum[i]) {
+					return new int[] { i, j };
 				}
 			}
 		}
-		
-		return new int[] {0};
+
+		return new int[] { 0 };
 	}
+
+	private static int[] twoSumOptimized(int[] arrNum, int target) {
+		HashMap hm = new HashMap();
+		for (int i = 0; i < arrNum.length; i++) {
+			hm.put(target - arrNum[i], i);
+		}
+
+		for (int i = 0; i < arrNum.length; i++) {
+			if (hm.containsKey(target - arrNum[i]) && (int)hm.get(target - arrNum[i]) != i) {
+				return new int[] { i, (int) hm.get(target - arrNum[i]) };
+				// hm.get()
+			}
+		}
+	    throw new IllegalArgumentException("No two sum solution");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arrNum = {3,3};
+		int[] arrNum = { 3, 1 };
 		int target = 6;
-		System.out.println("Two sum which gives " +  target + " is " + Arrays.toString(twoSum(arrNum,target)));
+		System.out.println("Two sum which gives " + target + " is " + Arrays.toString(twoSum(arrNum, target)));
+		System.out.println("Two sum which gives " + target + " is " + Arrays.toString(twoSumOptimized(arrNum, target)));
 
 	}
 
