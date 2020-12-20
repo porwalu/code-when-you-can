c5ddccf37f14efe36ee464e93ba61d23581d1ed6
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-20 23:48:58 +0530

    lcode easy - ToLowerCase

--- coding_ninja_prep/src/com/porwau/lcode/easy/ToLower.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/ToLower.java
@@ -1,22 +1,29 @@
 package com.porwau.lcode.easy;
 
+/**
+ * Converts String to lower case.
+ * only alphabets are changed and rest are left as is
+ * @author Utkarsh Porwal
+ *
+ */
 public class ToLower {
 
-    private static String toLowerCase(String str) {
+	private static String toLowerCase(String str) {
 		int strLength = str.length();
 		char[] ch = new char[strLength];
 		for (int i = 0; i < strLength; i++) {
 			if (str.charAt(i) <= 90 && str.charAt(i) >= 65) {
 				ch[i] = (char) (str.charAt(i) + 32);
-			}else {
+			} else {
 				ch[i] = str.charAt(i);
 			}
 		}
 		return String.valueOf(ch);
-    }
+	}
+
 	public static void main(String[] args) {
-String str = "HeLLo";
-System.out.println("Lower case value of " + str + " is " + toLowerCase(str));
+		String str = "HeLLo";
+		System.out.println("Lower case value of " + str + " is " + toLowerCase(str));
 	}
 
 }
