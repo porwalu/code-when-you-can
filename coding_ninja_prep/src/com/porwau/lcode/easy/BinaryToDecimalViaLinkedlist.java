7a3ca194fa526ab8e89f4a6eb0f94776129c7ac1
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-23 23:12:08 +0530

    lcode easy - Binary to decimal via linked list

--- coding_ninja_prep/src/com/porwau/lcode/easy/BinaryToDecimalViaLinkedlist.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/BinaryToDecimalViaLinkedlist.java
@@ -1,55 +1,57 @@
 package com.porwau.lcode.easy;
 
-class Node{
+class Node {
 	public Node(int value) {
 		this.value = value;
 	}
 	Node next;
 	int value;
-	
 }
-class BinaryLinkedList{
-	
+
+/**
+ * This implementation goes over a LinkedList which has binary numbers and
+ * converts them to decimal notation.
+ *
+ * @author Utkarsh Porwal
+ *
+ */
+class BinaryLinkedList {
+
 	Node head;
-	void insert(int value){//just after head
+
+	void insert(int value) {// just after head
 		Node newNode = new Node(value);
-		if(head == null) {
+		if (head == null) {
 			head = newNode;
 			head.next = null;
-		}else {
+		} else {
 			newNode.next = head.next;
 			head.next = newNode;
 		}
-		
 	}
-	
 }
 
 public class BinaryToDecimalViaLinkedlist {
+	/**
+	 * @param head - head of the LinkedList
+	 * @return - the decimal representation of binary number
+	 */
+	static int getDecimalValue(Node head) {
+		Node currNode = head;
+		int decimalNum = 0;
+		while (currNode != null) {
+			decimalNum = decimalNum * 2 + currNode.value;
+			currNode = currNode.next;
+		}
+		return decimalNum;
+	}
 
-
-    static int getDecimalValue(Node head) {
-        Node currNode = head;
-        int decimalNum = 0;
-        //int counter = 0;
-      while(currNode !=null){
-          decimalNum = decimalNum*2+currNode.value;
-          currNode = currNode.next;
-      }
-        return decimalNum;
-        
-    }
-	
 	public static void main(String[] args) {
 		BinaryLinkedList bll = new BinaryLinkedList();
 		bll.insert(1);
 		bll.insert(1);
 		bll.insert(0);
-//ABove will be inserted as 101
-		System.out.println("Binary to Decimal value is - "+ getDecimalValue(bll.head));
-
-		
-
+		//Above will be inserted as 101
+		System.out.println("Binary to Decimal value is - " + getDecimalValue(bll.head));
 	}
-
 }
