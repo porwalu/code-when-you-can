b8f17e7d0329b2d1c351036130b1cb104478a8a1
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-25 19:45:28 +0530

    lcode easy - Roman to Integer

---
+++ coding_ninja_prep/src/com/porwau/lcode/easy/RomanToInteger.java
@@ -0,0 +1,17 @@
+package com.porwau.lcode.easy;
+
+/**
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.<br>
+ * I can be placed before V (5) and X (10) to make 4 and 9. <br>
+ * X can be placed before L (50) and C (100) to make 40 and 90. <br>
+ * C can be placed before D (500) and M (1000) to make 400 and 900.<br>
+ * @author Utkarsh Porwal
+ */
+
+public class RomanToInteger {
+
+	public static void main(String[] args) {
+
+	}
+
+}
