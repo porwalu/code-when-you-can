491ec3dad667dfbce3452c62718346efa00d09d1
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-26 14:29:05 +0530

    lcode easy Longest Common Prefix

--- coding_ninja_prep/src/com/porwau/lcode/easy/LongestCommonPrefix.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/LongestCommonPrefix.java
@@ -1,7 +1,7 @@
 package com.porwau.lcode.easy;
 
 /**
- * Longest common prefix - only the prefix starting from begining for word.<br>
+ * Longest common prefix - only the prefix starting from beginning for word.<br>
  * if array is null, return blank.
  * 
  * @author Utkarsh Porwal
@@ -21,6 +21,7 @@ public class LongestCommonPrefix {
 		for (int i = 1; i < strings.length; i++) {
 			while (strings[i].indexOf(prefix) != 0) {// if it matches and it is a prefix, meaning, first index
 														// should always be 0.
+														// when prefix becomes empty, it also returns 0
 				prefix = prefix.substring(0, prefix.length() - 1);
 			}
 		}
