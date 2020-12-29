554aa6df53eb55b95761f49727734f207fef82f9
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-29 10:01:00 +0530

    lcode medium compare versions

--- coding_ninja_prep/src/com/porwau/lcode/medium/CompareVersions.java
+++ coding_ninja_prep/src/com/porwau/lcode/medium/CompareVersions.java
@@ -1,8 +1,10 @@
 package com.porwau.lcode.medium;
 
+//Leetcode 165. Compare Version Numbers
 //If version1 < version2, return -1.
 //If version1 > version2, return 1.
-//Otherwise, return 0.
+//Otherwise, return 0 if equal.
+//All leading zeros are ignored
 
 public class CompareVersions {
 
@@ -13,6 +15,17 @@ String v = "01";
 
 		String[] v1 = version1.split("\\.");
 		String[] v2 = version2.split("\\.");
+		boolean leftVersionGreater = true;
+		boolean leftVersionLonger=true;
+		int length = v2.length;
+		if(v1.length < v2.length) {
+			leftVersionLonger =false;
+			length = v1.length;
+		}
+		
+		for(int i = 0; i < length; i++) {
+			compare(v1[0],v2[0]);
+		}
 		if (Integer.parseInt(v1[0]) < Integer.parseInt(v2[0])) {
 			System.out.println("v1 < v2");
 			return -1;
@@ -21,6 +34,11 @@ String v = "01";
 			return 1;
 		}
 		return 1;
+	}
+
+	private static void compare(String string, String string2) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public static void main(String[] args) {
