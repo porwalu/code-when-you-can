464e8c8574c32285a2bd424d1b9442613cefadd6
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2020-12-30 10:01:00 +0530

    lcode medium compare versions

--- coding_ninja_prep/src/com/porwau/lcode/medium/CompareVersions.java
+++ coding_ninja_prep/src/com/porwau/lcode/medium/CompareVersions.java
@@ -19,10 +19,12 @@ public class CompareVersions {
 	}
 
 	static int compareVer(String version1, String version2) {
+		if (version1.equals(version2)) {
+			return 0;
+		}
 
 		String[] v1 = version1.split("\\.");
 		String[] v2 = version2.split("\\.");
-		int leftVersionGreater = 1;
 		int leftVersionLonger = 1;
 		int trackRev = 0;
 		int length = v2.length;
@@ -31,35 +33,36 @@ public class CompareVersions {
 			length = v1.length;
 		} else if (v1.length == v2.length) {
 			leftVersionLonger = 0;
-			length = v1.length;
-
 		}
 
-		for (int i = 0; i < length ; i++) {
-			trackRev = compare(v1[i].replaceAll("^0+(?!$)", ""), v2[i].replaceAll("^0+(?!$)", ""));
-			if(trackRev !=0) {break;}
+		for (int i = 0; i < length; i++) {
+			trackRev = compare(v1[i], v2[i]);
+			if (trackRev != 0) {
+				break;
+			}
 		}
 		// 1, 0, -1
 		if (trackRev == 0) {// both split revisions are equal.
-			if(leftVersionLonger == 0) {
+			if (leftVersionLonger == 0) {
 				return 0;
-			}else if(leftVersionLonger == 1) {//check for non zero in left string
-				trackRev = checkNonZeroRev(v1,v2.length);
-			}else {//check for non zero in right string.
-				trackRev = checkNonZeroRev(v2,v1.length);
+			} else if (leftVersionLonger == 1) {// check for non zero in left string
+				trackRev = checkNonZeroRev(v1, v2.length);
+			} else {// check for non zero in right string.
+				trackRev = checkNonZeroRev(v2, v1.length);
+				trackRev = trackRev > 0 ? -1 : 0;
 			}
 			// length is more and not just zeros then its longer. else return equal
-			//return leftVersionLonger;
-			
+			// return leftVersionLonger;
+
 		}
 		return trackRev;
 	}
 
 	private static int checkNonZeroRev(String[] version, int length) {
 		// TODO Auto-generated method stub
-		for(int i = length;i<version.length;i++) {
-			
-			if(Integer.parseInt(version[i].replaceAll("^0+(?!$)", "")) > 0){
+		for (int i = length; i < version.length; i++) {
+
+			if (Integer.parseInt(version[i]) > 0) {
 				return 1;
 			}
 		}
@@ -67,10 +70,8 @@ public class CompareVersions {
 	}
 
 	public static void main(String[] args) {
-		String version1 = "7.5.3.4";
-		String version2 = "7.5.3";
+		String version1 = "1";
+		String version2 = "1.1";
 		System.out.println(compareVer(version1, version2));
-
 	}
-
 }
