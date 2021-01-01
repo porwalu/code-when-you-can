9fb61c8abef3ccd80c62afdd5229fa2c4668ab5e
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-01-02 00:48:34 +0530

    lcode easy strStr

--- coding_ninja_prep/src/com/porwau/lcode/easy/StrStr.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/StrStr.java
@@ -2,15 +2,25 @@ package com.porwau.lcode.easy;
 
 /**
  * Leetcode - 28. Implement strStr()
+ * 
  * @author Utkarsh Porwal
  *
  */
 public class StrStr {
 
+	/**
+	 * @param haystack
+	 * @param needle
+	 * @return first index of needle in haystack
+	 */
 	private static int strStr(String haystack, String needle) {
 		if (needle.length() == 0) {
 			return 0;
 		}
+		if (!haystack.contains(needle)) {
+			return -1;
+		}
+
 		for (int i = 0; i < haystack.length(); i++) {
 			if (haystack.charAt(i) == needle.charAt(0)) {
 				// check substring.
@@ -23,7 +33,6 @@ public class StrStr {
 			}
 		}
 		return -1;
-
 	}
 
 	public static void main(String[] args) {
@@ -31,5 +40,4 @@ public class StrStr {
 		String haystack = "hello";
 		System.out.println("Index of needle in haystack is " + strStr(haystack, needle));
 	}
-
 }
