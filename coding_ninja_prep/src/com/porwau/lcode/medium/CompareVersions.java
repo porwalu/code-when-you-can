4ddfabee4fcd08feb8453dadc36394332178a383
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-01-01 22:57:06 +0530

    lcode medium compare versions

--- coding_ninja_prep/src/com/porwau/lcode/medium/CompareVersions.java
+++ coding_ninja_prep/src/com/porwau/lcode/medium/CompareVersions.java
@@ -8,15 +8,6 @@ package com.porwau.lcode.medium;
 //corner cases -  1.2.3.0 - 1.2.3, 1.2.3 - 1.2.3.0.1 - 
 
 public class CompareVersions {
-	private static int compare(String v1, String v2) {
-		if (Integer.parseInt(v1) > Integer.parseInt(v2)) {
-			return 1;
-		} else if (Integer.parseInt(v1) < Integer.parseInt(v2)) {
-			return -1;
-		}
-		return 0;
-
-	}
 
 	static int compareVer(String version1, String version2) {
 		if (version1.equals(version2)) {
@@ -25,45 +16,15 @@ public class CompareVersions {
 
 		String[] v1 = version1.split("\\.");
 		String[] v2 = version2.split("\\.");
-		int leftVersionLonger = 1;
-		int trackRev = 0;
-		int length = v2.length;
-		if (v1.length < v2.length) {
-			leftVersionLonger = -1;
-			length = v1.length;
-		} else if (v1.length == v2.length) {
-			leftVersionLonger = 0;
-		}
+		int l1 = v1.length;
+		int l2 = v2.length;
 
-		for (int i = 0; i < length; i++) {
-			trackRev = compare(v1[i], v2[i]);
-			if (trackRev != 0) {
-				break;
-			}
-		}
-		// 1, 0, -1
-		if (trackRev == 0) {// both split revisions are equal.
-			if (leftVersionLonger == 0) {
-				return 0;
-			} else if (leftVersionLonger == 1) {// check for non zero in left string
-				trackRev = checkNonZeroRev(v1, v2.length);
-			} else {// check for non zero in right string.
-				trackRev = checkNonZeroRev(v2, v1.length);
-				trackRev = trackRev > 0 ? -1 : 0;
-			}
-			// length is more and not just zeros then its longer. else return equal
-			// return leftVersionLonger;
-
-		}
-		return trackRev;
-	}
-
-	private static int checkNonZeroRev(String[] version, int length) {
-		// TODO Auto-generated method stub
-		for (int i = length; i < version.length; i++) {
-
-			if (Integer.parseInt(version[i]) > 0) {
-				return 1;
+		for (int i = 0; i < Math.max(l1, l2); i++) {
+			int rev1, rev2;
+			rev1 = i < l1 ? Integer.parseInt(v1[i]) : 0;
+			rev2 = i < l2 ? Integer.parseInt(v1[i]) : 0;
+			if (rev1 != rev2) {
+				return rev1 > rev2 ? 1 : -1;
 			}
 		}
 		return 0;
