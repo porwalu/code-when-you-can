fc8bfedea9914750a8bd47779a545a4586d8a4a2
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-01-02 16:07:00 +0530

    lcode easy best time to buy and sell stocks

--- coding_ninja_prep/src/com/porwau/lcode/easy/BuyAndSellStocks.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/BuyAndSellStocks.java
@@ -1,30 +1,38 @@
 package com.porwau.lcode.easy;
 
+/**
+ * LC - 121. Best Time to Buy and Sell Stock
+ * 
+ * @author Utkarsh Porwal Brute force - O(n^2) One Pass - O(n)
+ */
 public class BuyAndSellStocks {
 
 	public static void main(String[] args) {
-		System.out.println("Max profit is " + maxProfitBruteF(new int[] {7,1,5,3,6,3,10}));
-		System.out.println("Max profit is " + maxProfitOnePass(new int[] {7,1,5,3,6,3,10}));
+		System.out.println("Max profit is " + maxProfitBruteF(new int[] { 7, 1, 5, 3, 6, 3, 10 }));
+		System.out.println("Max profit is " + maxProfitOnePass(new int[] { 7, 1, 5, 3, 6, 3, 10 }));
 
 	}
 
 	private static int maxProfitBruteF(int[] prices) {
 		int profit = 0;
-		for (int i = 0; i < prices.length;i++) {
-			for(int j = i+1;j< prices.length; j++) {
-				if(prices[j] > prices[i] && (prices[j] - prices[i]) > profit) {
+		for (int i = 0; i < prices.length; i++) {
+			for (int j = i + 1; j < prices.length; j++) {
+				if (prices[j] > prices[i] && (prices[j] - prices[i]) > profit) {
 					profit = prices[j] - prices[i];
 				}
 			}
 		}
 		return profit;
 	}
-	private static int maxProfitOnePass(int [] prices) {
+
+	private static int maxProfitOnePass(int[] prices) {
 		int minPrice = Integer.MAX_VALUE;
 		int profit = 0;
 		for (int i = 0; i < prices.length; i++) {
-			if(prices[i] < minPrice) {minPrice = prices[i];}
-			if(prices[i] - minPrice > profit) {
+			if (prices[i] < minPrice) {
+				minPrice = prices[i];
+			}
+			if (prices[i] - minPrice > profit) {
 				profit = prices[i] - minPrice;
 			}
 		}
