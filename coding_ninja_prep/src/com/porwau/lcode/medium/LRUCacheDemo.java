03d05cb406d97405e7f576153164eb89c89b958f
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-01-03 16:51:36 +0530

    lcode medium LRU Cache

---
+++ coding_ninja_prep/src/com/porwau/lcode/medium/LRUCacheDemo.java
@@ -0,0 +1,20 @@
+package com.porwau.lcode.medium;
+
+/**
+ * Least Recently Used Cache.
+ * Here we evict the data which is oldest.
+ * @author Utkarsh Porwal
+ *
+ */
+public class LRUCacheDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class LRUCache{
+	
+}
