907f99c9fc4c7170a71311e59dc99d43dbf0e446
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-01-04 10:10:30 +0530

    lcode easy Single Number

--- coding_ninja_prep/src/com/porwau/lcode/easy/SingleNumber.java
+++ coding_ninja_prep/src/com/porwau/lcode/easy/SingleNumber.java
@@ -1,29 +1,42 @@
 package com.porwau.lcode.easy;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * 136. Single Number
+ * 
  * @author Utkarsh Porwal
  *
  */
 public class SingleNumber {
 
 	public static void main(String[] args) {
-		System.out.println("Single number is - " + singleNum(new int[] { 1, 1, 2, 3, 2 }));
+		System.out.println("Single number is - " + singleNum(new Integer[] { 1, 1, 2, 3, 2 }));
+		System.out.println("Single number is - " + singleNumXOR(new Integer[] { 1, 1, 2, 3, 2 }));
+
 	}
 
-	private static int singleNum(int[] nums) {
-		Map<Integer, Integer> m = new HashMap<>();
+	private static int singleNumXOR(Integer[] nums) {
+		int a = 0;
 		for (int i = 0; i < nums.length; i++) {
-			if (m.containsKey(nums[i])) {
-				m.remove(nums[i]);
+			a ^= nums[i];
+		}
+		return a;
+	}
+
+	private static int singleNum(Integer[] nums) {
+		List<Integer> unique = new ArrayList<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (unique.contains(nums[i])) {
+				unique.remove(nums[i]);
 			} else {
-				m.put(nums[i], 1);
+				unique.add(nums[i]);
 			}
 		}
-		return (int) m.keySet().toArray()[0];
+		return unique.get(0);
 	}
 }
