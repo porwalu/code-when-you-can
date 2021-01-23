943c45cdacaa8fb0da5d5dfff7d39b3f3d50983b
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-01-23 10:01:00 +0530

    Multithreading

--- coding_ninja_prep/src/com/porwau/concepts/concurrency/ThreadInterruptionDemo.java
+++ coding_ninja_prep/src/com/porwau/concepts/concurrency/ThreadInterruptionDemo.java
@@ -1,7 +1,6 @@
 package com.porwau.concepts.concurrency;
 
-
-class MyTask implements Runnable{
+class MyTask implements Runnable {
 	@Override
 	public void run() {
 		System.out.println("inside mytask thread");
@@ -14,15 +13,17 @@ class MyTask implements Runnable{
 		}
 	}
 }
+
 public class ThreadInterruptionDemo {
 
 	public static void main(String[] args) {
 		MyTask task1 = new MyTask();
 		Thread t1 = new Thread(task1);
 		t1.start();
+		System.out.println("Is interrupted? " + t1.isInterrupted());
+		t1.interrupt();
 		System.out.println("Main Thread finishes");
 
-		
 	}
 
 }
