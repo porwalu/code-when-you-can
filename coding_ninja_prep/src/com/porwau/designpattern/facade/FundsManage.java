7817c7f549c3faa81ba8fa96ea4bf44d9d15655f
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-02-19 10:01:00 +0530

    facadeDP

--- coding_ninja_prep/src/com/porwau/designpattern/facade/FundsManage.java
+++ coding_ninja_prep/src/com/porwau/designpattern/facade/FundsManage.java
@@ -1,15 +1,32 @@
 package com.porwau.designpattern.facade;
 
-public class FundsCheck {
-private double amount = 1000.00;
+public class FundsManage {
+	private double amount = 1000.00;
 
-public double getAmount() {
-	return amount;
-}
+	public double getAmount() {
+		return amount;
+	}
 
-public boolean checkFund() {
-	s
-}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
+	public void addCash(double amount) {
+		setAmount(getAmount() + amount);
+	}
 
+	public void redueCash(double amount) {
+		setAmount(getAmount() - amount);
+	}
+	
+	public boolean haveEnoughCash(double cashToWithdraw) {
+		
+		if(getAmount() >= cashToWithdraw) {
+			
+			return getAmount() >= cashToWithdraw;
+		} else {
+			System.out.println("You don't have enuf money.");
+			return false;
+		}
+	}
 }
