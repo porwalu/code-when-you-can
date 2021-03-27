f587a6ba65832799d01679c62b40fcd61adeace8
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-03-27 10:01:00 +0530

    lcode

---
+++ coding_ninja_prep/src/com/porwau/lcode/locked/TwoSumAgain.java
@@ -0,0 +1,34 @@
+package com.porwau.lcode.locked;
+
+import java.util.Arrays;
+
+//167. Two Sum II - Input array is sorted
+//Easy
+//O(n) 
+//Space complexity  O(1)
+public class TwoSumAgain {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] { 2, 7, 11, 15 };
+		int target = 90;
+		System.out.println(Arrays.toString(twoSumAgain(nums, target)));
+	}
+
+	private static int[] twoSumAgain(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+		while (left <= right) {
+			int sum = nums[left] + nums[right];
+			if (sum == target) {
+				return new int[] { left + 1, right + 1 };
+			} else if(sum > target) {
+				right = right - 1;				
+			} else {
+				left = left + 1;
+			}
+		}
+		return new int[] { -1, -1 };
+
+	}
+
+}
