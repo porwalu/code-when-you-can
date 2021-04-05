a81cfe9a14fb892bac2dae383c0d3af717b84314
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-04-05 10:01:00 +0530

    Multithreading

--- coding_ninja_prep/src/com/porwau/concepts/concurrency/ThreadDemo3.java
+++ coding_ninja_prep/src/com/porwau/concepts/concurrency/ThreadDemo3.java
@@ -24,17 +24,18 @@ class BankAccount implements Runnable {
 	private int balance = 100;
 
 	public void makeWithdrawal(int amount) {
-	synchronized(this) {
+//	synchronized(this) {
 		if(balance >=amount) {
 			System.out.println(amount + " bucks to be withdrawn by "+ Thread.currentThread().getName());
 			balance -= amount;
 		}else {
 			System.out.println("Money cant be withdrawn due to less balance. Please try later - " + Thread.currentThread().getName());
 		}
-	}
+	//}
 	}
 	@Override
 	public void run() {
+		System.out.println("Inside " + Thread.currentThread().getName() + "\'s thread.");
 		makeWithdrawal(75);
 		if(balance < 0) {
 			System.out.println("Money Overdrawn");
@@ -50,27 +51,27 @@ public class ThreadDemo3 {
 		aru.setName("Aru");
 		vasu.start();
 		aru.start();
-		
-		Email email = new Email();
-		Data data = new Data();
-		Thread t1 = new Thread(email);
-		t1.setName("email");
-		Thread t2 = new Thread(data);
-		t2.setName("data");
-		System.out.println(t1.getPriority());
-		System.out.println(t1.getStackTrace() + "  " + t1.getState());
-		// t1.start();
-		Thread.currentThread().sleep(10);
-		System.out.println(t1.getStackTrace() + "  " + t1.getState());
-		t2.start();
-		try {
-			//
-			t2.join(1);
-			t2.sleep(100);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		
+//		Email email = new Email();
+//		Data data = new Data();
+//		Thread t1 = new Thread(email);
+//		t1.setName("email");
+//		Thread t2 = new Thread(data);
+//		t2.setName("data");
+//		System.out.println(t1.getPriority());
+//		System.out.println(t1.getStackTrace() + "  " + t1.getState());
+//		// t1.start();
+//		Thread.currentThread().sleep(10);
+//		System.out.println(t1.getStackTrace() + "  " + t1.getState());
+//		t2.start();
+//		try {
+//			//
+//			t2.join(1);
+//			t2.sleep(100);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		System.out.println("Main Thread name " + Thread.currentThread().getName());
 
 
