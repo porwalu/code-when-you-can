5777165250d427736f7a082242fe11485d261222
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-04-13 10:01:00 +0530

    Multithreading

--- coding_ninja_prep/src/com/porwau/concepts/concurrency/ExecutorFrameworkDemo.java
+++ coding_ninja_prep/src/com/porwau/concepts/concurrency/ExecutorFrameworkDemo.java
@@ -3,6 +3,7 @@ package com.porwau.concepts.concurrency;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 class Task2 implements Runnable{
 static int i = 1;
@@ -25,6 +26,10 @@ public class ExecutorFrameworkDemo {
 		for(int i = 0; i <=100; i++) {			
 			executorA.execute(new Task2());
 		}
+		ScheduledExecutorService executorB = Executors.newScheduledThreadPool(cpuCores);
+		executorB.schedule(command, delay, unit);
+		executorB.scheduleAtFixedRate(command, initialDelay, period, unit);
+		executorB.scheduleWithFixedDelay(command, initialDelay, delay, unit);
 		System.out.println("Thread name in main is " + Thread.currentThread().getName());
 	}
 
