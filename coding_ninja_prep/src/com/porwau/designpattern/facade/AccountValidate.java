d77ff178c1ba433695496ce0807da098b280c05c
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-04-28 10:01:00 +0530

    facadeDP

--- coding_ninja_prep/src/com/porwau/designpattern/facade/AccountValidate.java
+++ coding_ninja_prep/src/com/porwau/designpattern/facade/AccountValidate.java
@@ -1,5 +1,15 @@
 package com.porwau.designpattern.facade;
 
 public class AccountValidate {
+	private int accountNumber = 12345;
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public boolean validateAccount(int accountNumber) {
+
+		return this.accountNumber == accountNumber;
+	}
 
 }
--- coding_ninja_prep/src/com/porwau/designpattern/facade/UserValidate.java
+++ coding_ninja_prep/src/com/porwau/designpattern/facade/UserValidate.java
@@ -1,7 +1,19 @@
 package com.porwau.designpattern.facade;
 
 public class UserValidate {
-	private String username;
-	private String password;
+	private String username = "utkarsh";
+	private String password = "password";
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public boolean validateUser(String username, String password) {
+		
+		return this.username == username  && this.password == password;
+	}
+	
 
 }
