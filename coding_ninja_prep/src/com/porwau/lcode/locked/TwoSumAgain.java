3fd1e65d4162850110d31d6e4f7035916986e1e4
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-06-02 08:40:54 +0530

    lcode

--- coding_ninja_prep/src/com/porwau/lcode/locked/TwoSumAgain.java
+++ coding_ninja_prep/src/com/porwau/lcode/locked/TwoSumAgain.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 //167. Two Sum II - Input array is sorted
 // this is add addition to the traditional two sum.
 //Addition is that the array is sorted.
-// space cokmplexity reduced to O(1)
+// space complexity reduced to O(1)
 //Easy
 //O(n) 
 //Space complexity  O(1)
 public class TwoSumAgain {
 
 	public static void main(String[] args) {
-		int[] nums = new int[] { 2, 7, 11, 15 };
-		int target = 90;
+		int[] nums = new int[] { 2, 5, 11, 15 };
+		int target = 10;
 		System.out.println(Arrays.toString(twoSumAgain(nums, target)));
 	}
 
 	private static int[] twoSumAgain(int[] nums, int target) {
 		int left = 0;
 		int right = nums.length - 1;
-		while (left <= right) {
+		while (left < right) {
 			int sum = nums[left] + nums[right];
 			if (sum == target) {
 				return new int[] { left + 1, right + 1 };
