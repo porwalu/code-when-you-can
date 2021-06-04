bf52d1577f1c26658c4d118f0465ae6e2b7e749b
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-06-04 10:01:00 +0530

    misc

---
+++ JI/src/com/porwau/test/SingletonTester.java
@@ -0,0 +1,43 @@
+package com.porwau.test;
+
+
+
+class DB{
+	
+	private static DB instance = new DB();
+	private static DB instance2;
+	
+	private DB() {
+		
+	}
+	
+	public static DB getDBInstance() {
+		return instance;
+	}
+	
+	public void test() {
+		instance = new DB();
+	}
+	
+	public  static  DB getInstanceLazy() {
+		if (instance2 == null) {
+			synchronized(DB.class) {
+				if(instance2 == null) {
+					instance2 = new DB();	
+				}
+			}
+		}
+		return instance2;
+	}
+	
+}
+
+
+public class SingletonTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
