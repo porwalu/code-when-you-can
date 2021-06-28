475c5eb6ad9f14847430fd7ad550ec68ccc9e65e
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-06-28 21:49:55 +0530

    Java 8 features

--- coding_ninja_prep/src/com/porwau/java8/ForEachDemo.java
+++ coding_ninja_prep/src/com/porwau/java8/ForEachDemo.java
@@ -11,11 +11,23 @@ public class ForEachDemo {
 		// TODO Auto-generated method stub
 		List<String> list = new ArrayList<>();
 
-		list = Arrays.asList("hello", "utkarsh", "ullas", "vasu");
+		list = Arrays.asList("hello", "utkarsh", "llas", "vasu");
+		List<Integer>listNum = Arrays.asList(1,12,14,15);
 
-		Stream<String> filterStream = list.stream().filter(p -> p.startsWith("u"));
-		System.out.println(filterStream.count());// this closes stream and cant be executed again.
-		System.out.println(filterStream.count());// this will give exception.
+		
+		list.forEach(p->System.out.println(p));
+		list.forEach(System.out::println);
+
+
+//		Stream<String> filterStream = list.stream().filter(p -> p.startsWith("u"));
+		
+		System.out.println(list.stream().filter(p -> p.startsWith("u")).equals("utkarsh"));
+		
+		System.out.println(listNum.stream().filter(n->n%2==0).max((a,b)->Integer.compare(a, b)));
+		
+
+		//System.out.println(filterStream.count());// this closes stream and cant be executed again.
+		//System.out.println(filterStream.count());// this will give exception.
 
 	}
 }
