ca80ee6d06cc07aee8f079ad63ab7b4f3af88962
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-06-28 21:58:13 +0530

    Java 8 features

--- coding_ninja_prep/src/com/porwau/java8/OptionalsDemo.java
+++ coding_ninja_prep/src/com/porwau/java8/OptionalsDemo.java
@@ -3,9 +3,8 @@ package com.porwau.java8;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
-public class ForEachDemo {
+public class OptionalsDemo {
 
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
@@ -13,20 +12,14 @@ public class ForEachDemo {
 		list = Arrays.asList("hello", "utkarsh", "llas", "vasu");
 		List<Integer>listNum = Arrays.asList(1,12,14,15);
 
-		
-		list.forEach(p->System.out.println(p));
-		list.forEach(System.out::println);
-
-
 //		Stream<String> filterStream = list.stream().filter(p -> p.startsWith("u"));
 		
-		System.out.println(list.stream().filter(p -> p.startsWith("u")).equals("utkarsh"));
-		
+//		System.out.println(list.stream().filter(p -> p.startsWith("u")).equals("utkarsh"));
+		list.stream().filter(p -> p.startsWith("u")).forEach(System.out::println);
+	
 		System.out.println(listNum.stream().filter(n->n%2==0).max((a,b)->Integer.compare(a, b)));
 		
 
-		//System.out.println(filterStream.count());// this closes stream and cant be executed again.
-		//System.out.println(filterStream.count());// this will give exception.
-
 	}
+
 }
